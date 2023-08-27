0428292b81fb18518e7fbc9277aedffe21dd5f12
Giancarlo <giancarlo.scalisi@dci.education>
2023-08-27 20:27:16 +0200

    routes bearbeitet

--- src/components/HeroImage2.jsx
+++ src/components/HeroImage2.jsx
@@ -1,9 +1,18 @@
 import "./HeroImage2Styles.css";
 
-import React from "react";
+import React, { Component } from "react";
 
-const HeroImage2 = () => {
-  return <div>HeroImage2</div>;
-};
+class HeroImage2 extends Component {
+  render() {
+    return (
+      <div className="hero-img">
+        <div className="heading">
+          <h1>{this.props.heading}</h1>
+          <p>{this.props.text}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default HeroImage2;
--- src/routes/Music.jsx
+++ src/routes/Music.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HeroImage2 from "../components/HeroImage2";
 
 const Music = () => {
   return (
     <div>
       <Navbar />
-      <HeroImage2 />
+      <HeroImage2 heading="MUSIC" text="My released Songs " />
       <Footer />
     </div>
   );
